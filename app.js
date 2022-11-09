77408d9c2259ff11528b2b0cf2439b1c99b87774
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-09 15:39:26 -0800

    added display functions

--- app.js
+++ app.js
@@ -9,18 +9,32 @@ const beanieSelect = document.getElementById('beanie-select');
 /* Get DOM Elements */
 /* State */
 let beanies = [];
-let astroSign = [];
+let astroSigns = [];
 
 /* Events */
 window.addEventListener('load', async () => {
     findBeanies();
 
     const response = await getAstroSign();
-    astroSign = response.data;
+    astroSigns = response.data;
 
-    displayBeanieOptions();
+    displayBeaniesOptions();
 });
 
 /* Display Functions */
+function displayBeanies() {
+    beanieList.innerHTML = '';
 
+    for (let beanie of beanies) {
+        const beaniesEl = renderBeanie(beanies);
+        beanieList.append(beaniesEl);
+    }
+}
+
+function displayBeaniesOptions() {
+    for (let astroSign of astroSigns) {
+        const astroEl = renderAstroSign(astroSign);
+        beanieSelect.append(astroEl);
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
