bc6809bec43d7694f1b845ec67db44cf62b438f8
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-09 14:32:10 -0800

    added export function

---
+++ fetch-utils.js
@@ -0,0 +1,15 @@
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// export async functions that fetch data
+export async function getBeanies(astroSign) {
+    let query = client.from('beanie_babies').select('*').order('releasedYear').limit(100);
+    if (astroSign) {
+        query = query.eq('astroSign', astroSign);
+    }
+    const response = await query;
+    return response;
+}
